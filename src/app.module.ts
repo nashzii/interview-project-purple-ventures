5c8585f3c288ebf31fa36d5e9265fe92172c0f57
Nuttajon Buraphapipop <nuttajon@hotmail.com>
2023-08-16 13:07:35 +0700

    chore: add docker file

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://admin:admin123@localhost:27017'),
+    MongooseModule.forRoot('mongodb://admin:admin123@mongodb:27017'),
     InterviewModule,
     UserModule,
     CommentModule,
